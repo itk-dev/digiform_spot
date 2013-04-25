c6132557d78cd5147660162ca25704c4771cdabd
bombycilla <sgl@aarhus.dk>
2013-04-25 18:40:56 +0200

    new variable-names and comments

--- library/scripts.js
+++ library/scripts.js
@@ -10,13 +10,12 @@ var myConfig = {
   max_image_height : 500,
   image_border : 3, // pixels
   space_between_images : 0.20, // procent
-  number_of_images : 3, // visible
-  id_prefix_images : 'imb', // some unique chars
-  animation : 1000, 
+  visible_images : 3, // synlige
+  set_of_images: 3, // de synlige sæt, før/aktivt/efter
+  id_prefix_images : 'imb', // tilfældige unikke tegn
+  animation : 1000,
   opacity : 0.8
 };
-
-var SET_OF_IMAGES = 3; // the visible set og one before/after
 
 Array.prototype.shuffle = function () {
   for (var i = this.length - 1; i > 0; i--) {
@@ -30,24 +29,23 @@ Array.prototype.shuffle = function () {
 
 function show_banner (sid) {
 
-    // handle nearly empty lists
-    if ( spotdata.list[sid].length < SET_OF_IMAGES * myConfig.number_of_images ) return;
+    // håndtér mindre lister
+    if ( spotdata.list[sid].length < myConfig.set_of_images * myConfig.visible_images ) carousel.small_list = true;
 
     // bland listen af numre
     spotdata.list[sid].shuffle();
 
-    // create banner-html
+    // dan banner-html
     var s = '';
     $.each( spotdata.list[sid], function( key, isbn ) {
-
       s += '<li><img id="' + myConfig.id_prefix_images + key + '" data-isbn="' + isbn + '" src="' + myConfig.folder + spotdata.isbn[isbn].i + '" width="' + myConfig.max_image_width + '" height="' + myConfig.max_image_height + '" alt="" /></li>'
-      if ( key >= SET_OF_IMAGES * myConfig.number_of_images - 1) return false;
+      if ( key >= myConfig.set_of_images * myConfig.visible_images - 1) return false;
     });
 
     var el = document.createElement('ul');
     $(el).html(s);
 
-    // slet tidligere carousel - er det nødvendigt?
+    // slet tidligere carousel
     if(carousel.obj) $('.imagebanner').jcarousel('destroy');
 
     // overfør data
@@ -57,32 +55,33 @@ function show_banner (sid) {
     carousel.obj = $('.imagebanner').jcarousel({ 'wrap': 'circular','animation': { 'duration': myConfig.animation, 'easing':   'easeInOutCubic'  } });
 
     // initialiser pointer
-    carousel.max_pointer = Math.floor( spotdata.list[sid].length / myConfig.number_of_images );
+    carousel.max_pointer = Math.floor( spotdata.list[sid].length / myConfig.visible_images );
     carousel.current_sid = sid;
-    
-    // set the last set of images to the last images in the total list
+
+    // det sidste sæt skal indeholde de sidste billeder i listen - dette trick ordner dette
     carousel.pointer = 1;
     update_li_content(-1);
 
-    carousel.scroll_in_action = 0;
+    carousel.scroll_in_action = false;
 
+    // opdater størrelser i css
     banner_recalculate();
 }
 
 function banner_recalculate(onlyImg) {
-  // the banner_width = (width+border) * number_of_images + (margin between images) * ( number_of_images - 1 )
+  // banner_width = (width+border) * visible_images + (margin between images) * ( visible_images - 1 )
   // margin between images = (some factor) * (width+border)
-  // calculate width...
+  // beregn width
 
   var banner_width = $('.imagebanner').outerWidth(true);
 
-  var new_image_width_and_border = Math.floor( banner_width / ( myConfig.number_of_images +  myConfig.space_between_images * (myConfig.number_of_images -1 ) ) )
+  var new_image_width_and_border = Math.floor( banner_width / ( myConfig.visible_images +  myConfig.space_between_images * (myConfig.visible_images -1 ) ) )
   var new_image_width = new_image_width_and_border - 2 * myConfig.image_border
   var new_image_margin = Math.floor( myConfig.space_between_images * new_image_width_and_border )
 
   var new_image_height =  Math.floor( new_image_width * myConfig.max_image_height / myConfig.max_image_width )
 
-  var new_banner_width = myConfig.number_of_images * new_image_width_and_border + ( myConfig.number_of_images -1 ) * new_image_margin
+  var new_banner_width = myConfig.visible_images * new_image_width_and_border + ( myConfig.visible_images -1 ) * new_image_margin
   var new_banner_margin = Math.floor(( banner_width - new_banner_width ) / 2);
   var new_banner_height = new_image_height + 2 * myConfig.image_border
 
@@ -95,20 +94,24 @@ function banner_recalculate(onlyImg) {
 }
 
 function modulo(a,b) {
-  // helper function - handle negative numbers
+  // håndterer negative tal (i modsætning til %)
   var res = a % b;
   return res < 0 ? res + b : res;
 }
 
 function update_li_content(offset){
-  // update one of the 3 set of images (before, current, next)
+  // håndterer visningen af den lange liste
+
+  // skip hvis listen er lille
+  if(carousel.small_list) return;
 
   carousel.pointer += offset;
 
-  var list_id = myConfig.number_of_images * modulo( carousel.pointer+offset, carousel.max_pointer)
-  var set_id = myConfig.number_of_images * modulo( carousel.pointer+offset, SET_OF_IMAGES)
+  var list_id = myConfig.visible_images * modulo( carousel.pointer+offset, carousel.max_pointer)
+  var set_id = myConfig.visible_images * modulo( carousel.pointer+offset, myConfig.set_of_images)
 
-  for ( i=0; i < myConfig.number_of_images; i++) {
+  // udskift det resp. set med de nye billeder fra listen
+  for ( i=0; i < myConfig.visible_images; i++) {
     var isbn = spotdata.list[carousel.current_sid][list_id+i]
     var src  = myConfig.folder + spotdata.isbn[isbn].i
     $('#' + myConfig.id_prefix_images + (set_id+i)).attr('src', src ).data('isbn', isbn);
@@ -116,14 +119,19 @@ function update_li_content(offset){
 }
 
 function scroll(evnt){
+  // håndter skiftet af billeder
+
+  // karusellen skifter så nulstil idletime
   carousel.idleTime=0;
+
+  // fortsæt kun hvis der ikke er en aktiv scroll
   if(!carousel.scroll_in_action) {
-     carousel.scroll_in_action = 1;
+     carousel.scroll_in_action = true;
      $('.navbutton').css('opacity', myConfig.opacity);
      update_li_content(evnt.data.offset);
 
-     var relative_offset = ( evnt.data.offset > 0 ? '+=' : '-=' ) + myConfig.number_of_images;
-     $('.imagebanner').jcarousel('scroll', relative_offset,  true, function() { carousel.scroll_in_action = 0; $('.navbutton').css('opacity', 1); } )
+     var relative_offset = ( evnt.data.offset > 0 ? '+=' : '-=' ) + myConfig.visible_images;
+     $('.imagebanner').jcarousel('scroll', relative_offset,  true, function() { carousel.scroll_in_action = false; $('.navbutton').css('opacity', 1); } )
   }
   return false;
 }
@@ -150,14 +158,14 @@ function create_events() {
 
     // inaktiv-checkeren...
     carousel.idleTime = 0;
-    setInterval(function() { carousel.idleTime+= 5; if(carousel.idleTime>10) { $('.imagebanner-right').click();} }, 5000);
+    setInterval(function() { carousel.idleTime+= 3; if(carousel.idleTime>=9) { $('.imagebanner-right').click();} }, 3000);
     $("body").on('mousemove', function (e) { carousel.idleTime = 0; });
     $("body").on('keypress',  function (e) { carousel.idleTime = 0; });
 
-    // resize will trigger new size of banner
+    // resize vil ændre størrelser som hermed opdateres
     $(window).on('resize', banner_recalculate);
 
-    // click on img-elements should trigger popup
+    // klik på billede skal trigge popup
     $(".imagebanner").on("click", "img", function(event){ show_popupbox($(this).data('isbn'));return false; });
 
 }
@@ -246,7 +254,7 @@ $(document).ready(function(){
   // imagebanner
   show_banner(spotdata.first);
 
-  // initialiser evetns
+  // initialiser events
   create_events();
 
   // submit
